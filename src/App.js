528f2e30de2deb1b75d57f70e7464e38ef338643
pseudocod <mocanvladinfo@gmail.com>
2024-01-05 13:54:08 +0200

    Initial commits for the second branch

--- src/App.js
+++ src/App.js
@@ -7,27 +7,43 @@ import Transfers from "./components/Transfers";
 import Loan from "./components/Loan";
 import Close from "./components/Close";
 import LogoutTimer from "./components/LogoutTimer";
-
+import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { auth } from "./config/firebase";
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <>
       <Header />
-      <main className="app">
-        {/* BALANCE */}
-        <Balance />
-        {/* MOVEMENTS */}
-        <Movements />
-        {/* SUMMARY */}
-        <Summary />
-        {/* OPERATION: TRANSFERS */}
-        <Transfers />
-        {/* OPERATION: LOAN */}
-        <Loan />
-        {/* OPERATION: CLOSE */}
-        <Close />
-        {/* LOGOUT TIMER */}
-        <LogoutTimer />
-      </main>
+      {user && (
+        <main className="app">
+          {/* BALANCE */}
+          <Balance />
+          {/* MOVEMENTS */}
+          <Movements />
+          {/* SUMMARY */}
+          <Summary />
+          {/* OPERATION: TRANSFERS */}
+          <Transfers />
+          {/* OPERATION: LOAN */}
+          <Loan />
+          {/* OPERATION: CLOSE */}
+          <Close />
+          {/* LOGOUT TIMER */}
+          <LogoutTimer />
+        </main>
+      )}
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,96 @@
-import React, { useState } from "react";
-import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState, useEffect } from "react";
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { GoogleLogo, SignOut, SignIn } from "@phosphor-icons/react";
+
 function Header() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
-  console.log(auth?.currentUser?.email);
+  // console.log(auth?.currentUser?.email);
 
-  const signIn = async () => {
-    console.log(email, password);
-    await createUserWithEmailAndPassword(auth, email, password);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []); // Run this effect only once during component mount
+
+  console.log(user?.email);
+
+  const signIn = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <nav>
       <p className="welcome">Log in to get started</p>
       <img src="/images/logo.png" alt="Logo" className="logo" />
       <form className="login">
-        <input
-          type="email"
-          placeholder="Email"
-          className="login__input login__input--user"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="login__input login__input--pin"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={signIn} className="login__btn">
-          →
-        </button>
+        {!user && (
+          <>
+            <input
+              type="email"
+              placeholder="Email"
+              className="login__input login__input--user"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="login__input login__input--pin"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </>
+        )}
+        {!user ? (
+          <button type="submit" onClick={signIn} className="login__btn">
+            <SignIn size={32} />
+          </button>
+        ) : null}
+        {!user ? (
+          <button
+            className="login__btn"
+            type="button"
+            onClick={signInWithGoogle}
+          >
+            <GoogleLogo size={32} />
+          </button>
+        ) : null}
+        {user ? (
+          <button className="login__btn" onClick={logout}>
+            <SignOut size={32} />
+          </button>
+        ) : null}
       </form>
     </nav>
   );
